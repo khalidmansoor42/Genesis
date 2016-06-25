7d44905d8dfe28d2fbdfdea03119c562650438e9
zainGohar <zainsheikh434@gmail.com>
2016-06-25 12:45:24 +0500

    fIRST  ReportsCommit

---
+++ Reports_BCL/Rptctl_BCL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reports_DAL;
+using System.Data;
+
+namespace Reports_BCL
+{
+    public class Rptctl_BCL
+    {
+        private RptCtl gObjRptCtl_DAL = new RptCtl();
+        public DataSet GetDataTreeList()
+        {
+            return gObjRptCtl_DAL.GetDataTreeLst();
+        }
+    }
+}
